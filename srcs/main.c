f34a2c262075e18852bf379e70ae08c4c73a52a2
lowatell <lowatell@MacBook.lan>
2025-01-17 08:28:28 +0100

    get path and test access

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: lowatell <lowatell@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 14:22:32 by lowatell          #+#    #+#             */
-/*   Updated: 2025/01/15 22:27:25 by lowatell         ###   ########.fr       */
+/*   Updated: 2025/01/17 08:26:52 by lowatell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +15,11 @@
 int main(int ac, char **av, char **env)
 {
 	t_pipex	bag;
+	int		i;
 
-    parsing(ac, av);
+    if (!parsing(ac, av))
+		return (0);
 	init_all(av, env, &bag);
+	i = access(ft_strjoin(bag.path, "cat ."), X_OK);
+	ft_printf("%s is %d\n", bag.cmd[0], i);
 }
--- srcs/parsing.c
+++ srcs/parsing.c
@@ -6,7 +6,7 @@
 /*   By: lowatell <lowatell@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 14:30:08 by lowatell          #+#    #+#             */
-/*   Updated: 2025/01/15 22:27:56 by lowatell         ###   ########.fr       */
+/*   Updated: 2025/01/17 08:19:12 by lowatell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,5 +20,5 @@ int parsing(int ac, char **av)
         return (0);
     }
 	ft_strlen(av[0]);
-    return (0);
+    return (1);
 }
