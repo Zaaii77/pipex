83ccbb621072219f0a8723e771f0a4dbfe4a16fd
lowatell <lowatell@MacBook.lan>
2025-01-25 03:03:04 +0100

    delete init_all

--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: lowatell <lowatell@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 14:22:32 by lowatell          #+#    #+#             */
-/*   Updated: 2025/01/24 17:43:51 by lowatell         ###   ########.fr       */
+/*   Updated: 2025/01/25 03:02:00 by lowatell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -19,9 +19,6 @@ int main(int ac, char **av, char **env)
 {
 	t_pipex	bag;
 
-	parsing(ac, av, env, &bag);
-    // if (!parsing(ac, av, env, &bag))
-	// 	return (0);
-	ft_printf("bag->path=%s\n", bag.path[0]);
-	ft_printf("bag->cmd1.pathcmd = %s\nbag->cmd2.pathcmd = %s\n", bag.cmd1.pathcmd, bag.cmd2.pathcmd);
+    if (!parsing(ac, av, env, &bag))
+ 		return (0);
 }
