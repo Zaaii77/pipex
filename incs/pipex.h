4487d5a4c2b1f38f4af705693ce0cf6adeb47815
lowatell <lowatell@MacBook.lan>
2025-01-31 11:31:19 +0100

    restart

--- incs/pipex.h
+++ incs/pipex.h
@@ -6,7 +6,7 @@
 /*   By: lowatell <lowatell@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 14:11:55 by lowatell          #+#    #+#             */
-/*   Updated: 2025/01/30 18:39:38 by lowatell         ###   ########.fr       */
+/*   Updated: 2025/01/31 11:31:08 by lowatell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -18,20 +18,5 @@
 # include <fcntl.h>
 # include "../libft/incs/libft.h"
 
-typedef struct s_cmd
-{
-	char	**cmd;
-	char	*pathcmd;
-	char	*file;
-}	t_cmd;
-
-typedef struct s_pipex
-{
-	char			**path;
-	struct s_cmd	cmd1;
-	struct s_cmd	cmd2;
-}	t_pipex;
-
-t_pipex	*parsing(int ac, char **av, char **env, t_pipex *bag);
 
 #endif
--- srcs/main.c
+++ srcs/main.c
@@ -6,7 +6,7 @@
 /*   By: lowatell <lowatell@student.s19.be>         +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2025/01/15 14:22:32 by lowatell          #+#    #+#             */
-/*   Updated: 2025/01/30 18:40:28 by lowatell         ###   ########.fr       */
+/*   Updated: 2025/01/31 11:29:43 by lowatell         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,13 +14,5 @@
 
 int	main(int ac, char **av, char **env)
 {
-	t_pipex	*bag;
-
-	bag = NULL;
-	bag = (t_pipex *)malloc(sizeof(bag));
-	bag = parsing(ac, av, env, bag);
-	if (!bag)
-		return (0);
-	ft_printf("%s, %s", bag->cmd1.cmd, bag->cmd1.file);
-	return (0);
+	
 }
